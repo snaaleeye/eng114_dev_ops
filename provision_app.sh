93802d6fced2409bce3974633312fc009b0f42ca
Sharmake Naaleeye <sharmakenaaleeye@Sharmakes-MacBook-Air.local>
2022-06-07 17:30:33 +0100

    update

--- provision_app.sh
+++ provision_app.sh
@@ -1,9 +1,34 @@
-cd app/app/app
+#!/bin/bash
 
-sudo echo "export DB_HOST=mongodb://192.168.56.15:27017/posts" >> ~/.bashrc
+#update and upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-sudo npm start
+#install nginx
+sudo apt-get install nginx -y
 
-node seeds/seed.js # do this if page loads with missing info
+#start nginx and make sure it runs from boot
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
-sudo npm start -d
+#Install Node.js 6.x repositorynpm 
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+
+#set up the enviroment variable for DB_HOST
+sudo echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> /etc/bash.bashrc
+source ~/.bashrc
+
+#Install Node.js and npm
+apt-get install -y nodejs
+npm install pm2 -g
+
+#copy over new default and run it
+sudo rm -rf /etc/nginx/sites-available/default
+sudo cp default /etc/nginx/sites-available/
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+#navigate to app folder, instal npm and run the app
+cd app/app
+npm install
+npm start -d
--- provision_db.sh
+++ provision_db.sh
@@ -1,27 +1,25 @@
+!/bin/bash
+
+#get mongo_db keys
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 
+#update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo apt-get install mongodb-org=3.2.20 -y
+
+##install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
-# To check if installation has been successful
-
 mongo --eval 'db.runCommand({ connectionStatus: 1 })'
 
-sudo systemctl status mongod
-
-sudo echo "export DB_HOST=mongodb://192.168.56.15:27017/posts" >> ~/.bashrc
-
-source ~/.bashrc
-
-cd /etc mongod.config
-
+#copy over new mongodb config and run it
+sudo rm -rf /etc/mongod.conf
+sudo cp mongod.conf /etc/mongod.conf
 sudo systemctl restart mongod
-
+sudo systemctl enable mongod
